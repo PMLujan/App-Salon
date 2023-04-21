13962842b6b913b231590e1f420734cc53d114a7
PMLujan <lujan_paredes@hotmail.com.ar>
2023-04-21 20:38:50 -0300

    Pagina login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,31 @@
-<h1>desde login </h1>
+<h1 class="nombrePagina">Login</h1>
+<p class="descripcionPagina">Inicia Sesión con tus Datos</p> <!-- las clases estan en tipografia-->
+
+<form class="formulario" method="post" action="/">
+    <div class="campo">
+        <label for="email">E-mail</label>
+        <input 
+                type="email"
+                id="email"
+                placeholder="Tu E-mail"
+                name="email"
+        />
+    </div>
+    <div class="campo">
+        <label for="password">Password</label>
+        <input 
+                type="password" 
+                id="password"
+                placeholder="Tu Password"
+                name="password"
+        />
+    </div>
+    <input class="boton" type="submit" value="Iniciar Sesión">
+
+    <div class="acciones">
+        <a href="/crear-cuenta">¿Aún no tienes una cuenta? <span>Crear cuenta</span> </a>
+        <a href="/olvide"> ¿Olvidaste tu password? </a>
+    </div>
+
+
+</form>
